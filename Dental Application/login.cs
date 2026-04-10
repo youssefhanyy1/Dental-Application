5f46a3e14f5912c0e26e931e441b0c4e0eb81f6c
youssef hany <hany67334@gmail.com>
2026-04-10 11:50:05 +0200

    Add project files.

---
+++ Dental Application/login.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dental_Application
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitPIC_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            guna2ShadowForm1.SetShadowForm(this);
+        }
+
+        private void guna2Button2_Click(object sender, EventArgs e)
+        {
+     
+
+        }
+    }
+}
